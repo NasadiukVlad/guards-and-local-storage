5b1dd4bf781233b021af2ddff0c5d12f1e636ef9
Vladislav Nasadiuk <vladyslav.nasadiuk@sigma.software>
2018-11-19 11:05:36 +0200

    remove uselss code

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -7,19 +7,6 @@ import {Router} from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
-  public orderId: number;
-
-  constructor(private router: Router) {}
-
-  public navigateToOrder() {
-    console.log(this.orderId);
-    this.router.navigate(['order-data', this.orderId]);
-  }
-
-  public navigateToAbout() {
-    this.router.navigate(['about'], {queryParams: {'customQuery': 6456, 'secondQueryParam': 200}});
-  }
 }
 
 
